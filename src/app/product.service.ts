e1d67a73505278fd828e193859b4f068413ea8dc
dakota.miller <dakotajmiller123@gmail.com>
2018-04-02 14:11:19 -0600

    creating service

---
+++ src/app/product.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class ProductService {
+  getAlbum(id:number){
+    this._http.get(this._albumUrl).response.json()
+  }
+  private _albumUrl = '../assets/album.json';
+  constructor(private _http: Http) { }
+
+}
